e56446d6b33b742fef42d21b8ceb0aefe9651a72
samuel <samuelsyameogo@gmail.com>
2020-03-02 15:01:39 -0800

    c
    c

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" T50");
+
+            int product = 100000 * 50;
+            Console.WriteLine(product);
+            Console.ReadLine();
+
+            int total = 298 + 25;
+            Console.WriteLine("298  plus 25  = " + total.ToString());
+            Console.ReadLine();
+
+            double quotient = 101.0 / 12.5;
+            Console.WriteLine(quotient);
+            Console.ReadLine();
+
+            bool trueOrFalse = 63 > 50;
+            Console.WriteLine(trueOrFalse.ToString());
+            Console.ReadLine();
+
+            int remainder = 13 % 7;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+
+
+    
+            
+
+        }
+    }
+}
+
